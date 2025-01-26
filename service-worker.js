07bf001d37b89b5b108d952ac63c7a8d5a2cd0e0
Ligul <ligul3d@mail.ru>
2025-01-26 08:09:10 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,10 @@
 self.addEventListener('fetch', (event) => {
+  const url = new URL(event.request.url);
+
+  // Пропускайте любые запросы, которые не относятся к вашему сайту
+  if (url.origin !== location.origin) {
+    return;
+  }
+
   event.respondWith(fetch(event.request));
 });
