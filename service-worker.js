bd8e112c35233c0979933ee02c033749a9434f47
ligul <ligul3d@mail.ru>
2025-01-26 22:31:30 +0400

    Rewrite to use iframe

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,9 @@
-self.addEventListener('fetch', (event) => {
-  const url = new URL(event.request.url);
+self.addEventListener("fetch", (event) => {
+    const url = new URL(event.request.url);
 
-  // Пропускайте любые запросы, которые не относятся к вашему сайту
-  if (url.origin !== location.origin) {
-    return;
-  }
+    if (url.origin !== location.origin) {
+        return;
+    }
 
-  event.respondWith(fetch(event.request));
+    event.respondWith(fetch(event.request));
 });
